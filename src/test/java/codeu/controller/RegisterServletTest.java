ba94e2b0bf860a79e6c82a534bd4a926f9eda2ab
Tishauna Wilson <25163556+TWilson2017@users.noreply.github.com>
2018-03-19 18:03:37 -0400

    Update RegisterServletTest.java
    Changing Indention's

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -1,5 +1,4 @@
 package codeu.controller;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,26 +8,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class RegisterServletTest {
+public class RegisterServletTest
+{
+  private RegisterServlet registerServlet;
+  private HttpServletRequest mockRequest;
+  private PrintWriter mockPrintWriter;
+  private HttpServletResponse mockResponse;
+ 
+  @Before
+  public void setup() throws IOException 
+  {
+    registerServlet = new RegisterServlet();
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockPrintWriter = Mockito.mock(PrintWriter.class);
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+    Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
+  }//setup
 
- private RegisterServlet registerServlet;
- private HttpServletRequest mockRequest;
- private PrintWriter mockPrintWriter;
- private HttpServletResponse mockResponse;
-
- @Before
- public void setup() throws IOException {
-   registerServlet = new RegisterServlet();
-   mockRequest = Mockito.mock(HttpServletRequest.class);
-   mockPrintWriter = Mockito.mock(PrintWriter.class);
-   mockResponse = Mockito.mock(HttpServletResponse.class);
-   Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
- }//setup
-
- @Test
- public void testDoGet() throws IOException, ServletException {
-   registerServlet.doGet(mockRequest, mockResponse);
-
-   Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET request.</h1>");
- }//testDoGet
+  @Test
+  public void testDoGet() throws IOException, ServletException
+  {
+    registerServlet.doGet(mockRequest, mockResponse);
+    Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET request.</h1>");
+  }//testDoGet
 }//RegisterServletTest
