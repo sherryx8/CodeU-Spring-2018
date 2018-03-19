b3ddab55e18ad096ee8e0db42211473249eb244f
Tishauna Wilson <25163556+TWilson2017@users.noreply.github.com>
2018-03-19 18:01:01 -0400

    Update RegisterServlet.java

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 /**
 * Servlet class responsible for user registration.
 */
-public class RegisterServlet extends HttpServlet {
-
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-     throws IOException, ServletException {
-
-   response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
- }//doGet
+public class RegisterServlet extends HttpServlet
+{
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException
+  { 
+    response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
+  }//doGet
 }//RegisterServlet
