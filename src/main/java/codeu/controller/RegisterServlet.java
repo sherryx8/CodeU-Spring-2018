20b0941b8cb315c14bce7112e27f4408906565fc
Tishauna Wilson <25163556+TWilson2017@users.noreply.github.com>
2018-04-01 23:29:34 -0400

    Update RegisterServlet.java

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -1,18 +1,11 @@
 package codeu.controller;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
-
 import javax.servlet.http.HttpServlet;
-
 import javax.servlet.http.HttpServletRequest;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.mockito.Mockito;
-
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
 
