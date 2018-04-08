95f4149ed19e62a74ef7ab4e90fcb7f5e40066b8
TsueiCat <catherinetsuei617@gmail.com>
2018-04-08 12:23:45 -0400

    fixed up comments and did some renaming
    Signed-off-by: TsueiCat <catherinetsuei617@gmail.com>

--- src/main/java/codeu/controller/ChatServlet.java
+++ src/main/java/codeu/controller/ChatServlet.java
@@ -56,40 +56,37 @@ public class ChatServlet extends HttpServlet {
   }
 
   /**
-   * Sets the ConversationStore used by this servlet. This function provides a
-   * common setup method for use by the test framework or the servlet's init()
-   * function.
+   * Sets the ConversationStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
    */
   void setConversationStore(ConversationStore conversationStore) {
     this.conversationStore = conversationStore;
   }
 
   /**
-   * Sets the MessageStore used by this servlet. This function provides a common
-   * setup method for use by the test framework or the servlet's init()
-   * function.
+   * Sets the MessageStore used by this servlet. This function provides a common setup method for
+   * use by the test framework or the servlet's init() function.
    */
   void setMessageStore(MessageStore messageStore) {
     this.messageStore = messageStore;
   }
 
   /**
-   * Sets the UserStore used by this servlet. This function provides a common
-   * setup method for use by the test framework or the servlet's init()
-   * function.
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
    */
   void setUserStore(UserStore userStore) {
     this.userStore = userStore;
   }
 
   /**
-   * This function fires when a user navigates to the chat page. It gets the
-   * conversation title from the URL, finds the corresponding Conversation, and
-   * fetches the messages in that Conversation. It then forwards to chat.jsp for
-   * rendering.
+   * This function fires when a user navigates to the chat page. It gets the conversation title from
+   * the URL, finds the corresponding Conversation, and fetches the messages in that Conversation.
+   * It then forwards to chat.jsp for rendering.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException, ServletException {
     String requestUrl = request.getRequestURI();
     String conversationTitle = requestUrl.substring("/chat/".length());
 
@@ -111,14 +108,14 @@ public class ChatServlet extends HttpServlet {
   }
 
   /**
-   * This function fires when a user submits the form on the chat page. It gets
-   * the logged-in username from the session, the conversation title from the
-   * URL, and the chat message from the submitted form data. It creates a new
-   * Message from that data, adds it to the model, and then redirects back to
-   * the chat page.
+   * This function fires when a user submits the form on the chat page. It gets the logged-in
+   * username from the session, the conversation title from the URL, and the chat message from the
+   * submitted form data. It creates a new Message from that data, adds it to the model, and then
+   * redirects back to the chat page.
    */
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException, ServletException {
 
     String username = (String) request.getSession().getAttribute("user");
     if (username == null) {
@@ -149,8 +146,13 @@ public class ChatServlet extends HttpServlet {
     // this removes any HTML from the message content
     String cleanedMessageContent = Jsoup.clean(messageContent, Whitelist.none());
 
-    String boldItalicsMessageContent = markDownToHTML(cleanedMessageContent);
-    Message message = new Message(UUID.randomUUID(), conversation.getId(), user.getId(), boldItalicsMessageContent,
+    String htmlTagMessageContent = markDownToHTML(cleanedMessageContent);
+    Message message = 
+        new Message(
+            UUID.randomUUID(),
+            conversation.getId(),
+            user.getId(),
+            htmlTagMessageContent,
         Instant.now());
 
     messageStore.addMessage(message);
@@ -163,7 +165,6 @@ public class ChatServlet extends HttpServlet {
   private final int MARKDOWN_BOLDITALIC_IDENTIFIER_LENGTH = "***".length();
   private final int MARKDOWN_BOLD_IDENTIFIER_LENGTH = "**".length();
   private final int MARKDOWN_ITALIC_IDENTIFIER_LENGTH = "*".length();
-  private final int MARKDOWN_ESCAPE_IDENTIFIER_LENGTH = "\\*".length();
   private final int BBCODE_COLOR_BEGIN_IDENTIFIER_LENGTH = "\\[color=".length() - 1;
   private final int BBCODE_COLOR_END_IDENTIFIER_LENGTH = "\\[/color\\]".length() - 2;
   private final int BBCODE_SIZE_BEGIN_IDENTIFIER_LENGTH = "\\[size=".length() - 1;
