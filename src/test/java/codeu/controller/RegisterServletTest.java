d0723f1f6c60039e1137f70dce9d0e78cfb1db90
Tishauna Wilson <25163556+TWilson2017@users.noreply.github.com>
2018-04-22 00:37:54 -0400

    Update RegisterServletTest
    Testing for RegisterServlet

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -1,19 +1,15 @@
 package codeu.controller;
-
 import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import codeu.model.data.Message;
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
@@ -39,7 +35,24 @@ public class RegisterServletTest {
     registerServlet.doGet(mockRequest, mockResponse);
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
   } //testDoGet
+  
+  @Test
+  public void testDoPost_UserNoMatch() throws IOException, ServletException {
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("Username Invalid");
+    Mockito.when(mockRequest.getAttribute("user")).thenReturn("test_username");
 
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    User Invalid_Username = new User (UUID.randomUUID(), "test_username", "password", Instant.now(), "About_Me_Text");
+    Mockito.when(mockUserStore.getUser("test_username")).thenReturn(Invalid_Username);
+    
+    RegisterServlet mockRegisterServlet = Mockito.mock(RegisterServlet.class);
+    registerServlet.setUserStore(mockUserStore);
+    
+    Mockito.verify(mockRegisterServlet, Mockito.never());
+    Mockito.verify(mockRequest).setAttribute("error", "Please enter only letters and numbers.");
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  } //testDoPost_UserNoMatch
+  
   @Test
   public void testDoPost_ExistingUser() throws IOException, ServletException {
     Mockito.when(mockRequest.getParameter("username")).thenReturn("testing username...");
