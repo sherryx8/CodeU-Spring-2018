53650a4db9f97ad7abe44652d44bcc7a52be93ec
Thar Min Htet <kianhtet@gmail.com>
2018-05-25 09:23:38 -0400

    Fixed comments in Logout servlet

--- src/main/java/codeu/controller/LogoutServlet.java
+++ src/main/java/codeu/controller/LogoutServlet.java
@@ -1,17 +1,3 @@
-// Copyright 2017 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package codeu.controller;
 
 import codeu.model.data.User;
@@ -62,11 +48,6 @@ public class LogoutServlet extends HttpServlet {
         response.sendRedirect("/login");
   }
 
-  /**
-   * This function fires when a user submits the logout form. It gets the username and password
-   * from the submitted form data, checks that they're valid, and either adds the user to the session
-   * so we know the user is logged in or shows an error to the user.
-   */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
--- src/main/webapp/WEB-INF/view/profile.jsp
+++ src/main/webapp/WEB-INF/view/profile.jsp
@@ -38,7 +38,7 @@ Collections.reverse(userMessages);
   <nav>
     <a id="navTitle" href="/">CodeU Chat App</a>
     <a href="/conversations">Conversations</a>
-      <% if (request.getSession().getAttribute("user") != null) { %>
+    <% if (request.getSession().getAttribute("user") != null) { %>
     <a>Hello <%= request.getSession().getAttribute("user") %>!</a>
     <% } else { %>
       <a href="/login">Login</a>
